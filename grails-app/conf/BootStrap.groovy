31574572c07e890c8038244271ad79398c96cb1a
EC2 Default User <ec2-user@ip-172-31-46-120.us-west-2.compute.internal>
2018-02-21 01:53:29 +0000

    Print starting up to stdout

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,7 +1,21 @@
+import javax.servlet.*
+import java.util.jar.*
+import java.text.*
+
 class BootStrap {
 
     def init = { servletContext ->
+        InputStream inputStream = servletContext.getResourceAsStream("/META-INF/MANIFEST.MF");
+        Manifest manifest = new Manifest(inputStream);
+        Attributes secondaryAttributes = manifest.getAttributes("Grails Application")
+        String sha =  secondaryAttributes.getValue('Implementation-Build-Number')
+
+        String ii = "curl 169.254.169.254/latest/meta-data/instance-id".execute().text
+        SimpleDateFormat format=new SimpleDateFormat ("yyyy-MM-dd:HH:mm:ss.SSS");
+        println "candlestick microcero $sha $ii ${format.format(new Date())}"
     }
+
     def destroy = {
     }
 }
+
